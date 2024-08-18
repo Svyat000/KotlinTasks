8d9cfec5927fc9d49f5b6cee84be7078f7e2a507
svyatoslav <sddrozdov000@gmail.com>
2024-08-18 18:39:57 +0300

    KS-1-5 refactor spaces

--- src/main/kotlin/lesson_1/lesson1task5.kt
+++ src/main/kotlin/lesson_1/lesson1task5.kt
@@ -4,15 +4,13 @@ fun main(){
 
     val secondInHour = 60
     val minutesInHour = 60
-    val seconds = 6480  //Невнимательно прочитал задание, подумал сюда любое количество секунд надо поставить)
+    val seconds = 6480
 
-    val secondFinish = seconds%secondInHour
-    val minutes = (seconds/secondInHour)%secondInHour
-    val hour = seconds/secondInHour/minutesInHour
+    val secondFinish = seconds % secondInHour
+    val minutes = (seconds / secondInHour) % secondInHour
+    val hour = seconds / secondInHour / minutesInHour
     println("$hour:$minutes:$secondFinish")
-   /* print(hour)
-    print(":"+minutes )
-    print(":"+secondFinish )*/
+
 
 
 }
