33f6d92959bddca2a3b1d96940141f11e73b3265
svyatoslav <sddrozdov000@gmail.com>
2024-08-18 12:51:00 +0300

    KS-2-4 buf arithmetic

---
+++ src/main/kotlin/lesson_2/lesson2task4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_2
+
+import kotlin.math.roundToInt
+
+fun main(){
+    val buf = 0.2
+
+    val crystalOre = 7
+    val metalOre = 11
+
+    val resultCrystalOre = crystalOre*buf
+    val resultMetalOre = metalOre*buf
+
+    println("crystalOre: $crystalOre")
+    println("BONUS crystalOre: "+ resultCrystalOre.roundToInt())
+    println("crystalOre: $metalOre")
+    println("BONUS crystalOre: "+ resultMetalOre.roundToInt())
+
+
+}
