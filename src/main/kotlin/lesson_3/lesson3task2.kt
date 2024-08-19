867216e14f4125fc407f520e5ce5a944fb5dd4aa
svyatoslav <sddrozdov000@gmail.com>
2024-08-19 11:37:28 +0300

    KS-3-2 made a string variables

---
+++ src/main/kotlin/lesson_3/lesson3task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main(){
+
+    val Surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    val newSurname = "Сидорова"
+
+    val yearFirst = 20
+    val yearSecond = 22
+
+    println("$Surname $name $patronymic $yearFirst лет")
+    println("$newSurname $name $patronymic $yearSecond лет")
+}
