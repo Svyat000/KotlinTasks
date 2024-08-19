b66795c968041daa46d5657aef5ab88146d1f065
svyatoslav <sddrozdov000@gmail.com>
2024-08-19 11:47:37 +0300

    KS-3-3 made a multiplication table

---
+++ src/main/kotlin/lesson_3/lesson3task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_3
+
+fun main(){
+
+    val number = 7
+
+    println("""
+        $number х 1  = ${number * 1} 
+        $number х 2  = ${number * 2}
+        $number х 3  = ${number * 3}
+        $number х 4  = ${number * 4}
+        $number х 5  = ${number * 5}
+        $number * 6  = ${number * 6}
+        $number * 7  = ${number * 7}
+        $number * 8  = ${number * 8}
+        $number * 9  = ${number * 9}
+        $number * 10 = ${number * 10}       
+    """.trimIndent())
+}
