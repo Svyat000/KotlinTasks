2b81ab98051bfef99aff7d5ecbef0377a4824117
svyatoslav <sddrozdov000@gmail.com>
2024-08-19 12:25:42 +0300

    KS-3-4 made chess moves

---
+++ src/main/kotlin/lesson_3/lesson3task4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main(){
+    var startMove = "E2"
+    var finishMove = "E4"
+    var countMove = 1
+
+    println("$startMove - $finishMove Ход № $countMove")
+
+    startMove = "D2"
+    finishMove = "D3"
+    countMove++
+    println("$startMove - $finishMove Ход № $countMove")
+
+
+}
