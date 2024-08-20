112ea527db6d4e2d34e131b371399d30785ec4d6
svyatoslav <sddrozdov000@gmail.com>
2024-08-20 08:45:16 +0300

    KS-2-5 refactor divider and spaces

--- src/main/kotlin/lesson_2/lesson2task5.kt
+++ src/main/kotlin/lesson_2/lesson2task5.kt
@@ -5,12 +5,10 @@ fun main(){
     val startSum = 70000
     val persentInYear = 16.7
     val depositTerm = 20
+    val divider = 100
 
-    val result = startSum * Math.pow(1 + persentInYear / 100, depositTerm.toDouble())
+    val result = startSum * Math.pow(1 + persentInYear / divider, depositTerm.toDouble())
 
     println(String.format("%.3f",result))
-
-
-
 
 }
