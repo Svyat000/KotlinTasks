27b5ab4433ca257082db133a53ecc852344e05e4
svyatoslav <sddrozdov000@gmail.com>
2024-08-20 08:36:38 +0300

    KS-2-3 refactor calculations and out

--- src/main/kotlin/lesson_2/lesson2task3.kt
+++ src/main/kotlin/lesson_2/lesson2task3.kt
@@ -3,17 +3,14 @@ package org.example.lesson_2
 fun main(){
     val startHour = 9
     val startMinutes = 39
-    val pathTime = 457
+    val pathTimeInMinutes = 457
     val minutesInHour = 60
+    val generalStartMinutes = (startHour * minutesInHour) + startMinutes
 
-    var finishHour = startHour + (pathTime / minutesInHour)
-    var finishMinutes = startMinutes + (pathTime % minutesInHour)
 
-    if(finishMinutes > minutesInHour) {
-        finishMinutes -= minutesInHour
-        finishHour += 1
-        println("$finishHour:$finishMinutes")
+    val finishHour = (generalStartMinutes + pathTimeInMinutes) / minutesInHour
+    val finishMinutes = (generalStartMinutes + pathTimeInMinutes) % minutesInHour
 
-    }
+    println("%02d:%02d".format(finishHour,finishMinutes))
 
 }
